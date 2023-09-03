bb8ec53e70d8593afb47664d3309e405fcde3bdd
C. Mangla <cmangla@users.noreply.github.com>
2023-09-03 18:07:29 +0100

    Table Scrapper Added

---
+++ table-scrape.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+for i in ('ing-forecasts.html', 'scotiabank-forecast-snapshot.html'):
+    dfs = pd.read_html(i)
+    for j in range(len(dfs)):
+        fn = i.removesuffix(".html") + "_" + str(j) + ".csv"
+        dfs[j].to_csv(fn)
