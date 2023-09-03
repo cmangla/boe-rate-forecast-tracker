824b1d4dd2537264af511e5f891aefec0fb30e94
C. Mangla <cmangla@users.noreply.github.com>
2023-09-03 18:17:22 +0100

    Drop empty rows

--- table-scrape.py
+++ table-scrape.py
@@ -5,4 +5,4 @@ for i in ('ing-forecasts.html', 'scotiabank-forecast-snapshot.html'):
     dfs = pd.read_html(i)
     for j in range(len(dfs)):
         fn = i.removesuffix(".html") + "_" + str(j) + ".csv"
-        dfs[j].to_csv(fn)
+        dfs[j].dropna(how='all').to_csv(fn)
