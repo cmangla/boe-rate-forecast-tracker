ab17d262f61bd87dd9aaf9fa3729d32921b39bcc
C. Mangla <cmangla@users.noreply.github.com>
2025-05-16 12:27:30 +0100

    table-scrape.py: ignore some fails

--- table-scrape.py
+++ table-scrape.py
@@ -2,7 +2,10 @@
 import pandas as pd
 
 for i in ('ing-forecasts.html', 'scotiabank-forecast-snapshot.html'):
-    dfs = pd.read_html(i)
-    for j in range(len(dfs)):
-        fn = i.removesuffix(".html") + "_" + str(j) + ".csv"
-        dfs[j].dropna(how='all').to_csv(fn)
+    try:
+        dfs = pd.read_html(i)
+        for j in range(len(dfs)):
+            fn = i.removesuffix(".html") + "_" + str(j) + ".csv"
+            dfs[j].dropna(how='all').to_csv(fn)
+    except:
+        pass
